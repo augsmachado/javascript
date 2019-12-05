78560e8b9df2871e4e4e3936de26d0db78bbefd5
Augusto Machado <augustomachadoramos@gmail.com>
2019-12-05 17:08:12 -0300

    Add function flatMap

---
+++ javascript/array/concat.js
@@ -0,0 +1,10 @@
+const filhas = ['Ualeskah', 'Cibalena']
+const filhos = ['Uoxinton', 'Uesclei']
+
+// concat par unir filhos e filhas
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [
+    [6, 7]
+]))
