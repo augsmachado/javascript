993d3e3a6dc700f04239c9060a3cb890df3e794c
Augusto Machado <augustomachadoramos@gmail.com>
2019-12-09 11:17:21 -0300

    Add sistemaModulosNode

---
+++ nodeJS/sistemaModulos/moduloA.js
@@ -0,0 +1,6 @@
+// vc esta exportando um arquivo
+this.ola = 'Fala Pessoal'
+
+exports.bemVindo = 'Bem vindo ao node!'
+
+module.exports.ateLogo = 'Ate o proximo exemplo'
---
+++ nodeJS/sistemaModulos/moduloB.js
@@ -0,0 +1,9 @@
+// quado vc substitui o obj padrao, deve utilizar o metodo abaixo
+// isto eh module.exports = {}
+// caso contrario dara um erro
+module.exports = {
+    bomDia: 'Bom dia',
+    boaNoite() {
+        return 'Boa noite'
+    }
+}
